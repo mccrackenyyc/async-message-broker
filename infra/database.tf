dfcb9a6a728ad7d03c5e76dcf4ccf731623aaa22
mccrackenyyc <smccracken@live.ca>
2023-03-08 14:35:26 -0700

    Database configuration

---
+++ infra/database.tf
@@ -0,0 +1,62 @@
+resource "azurerm_resource_group" "amb_sql_rg" {
+  name     = "amb-sql-rg-${var.env_name}"
+  location = "Canada Central"
+
+  tags = {
+    tag = var.exampletag
+  }
+}
+
+resource "azurerm_storage_account" "amb_sql_logs" {
+  name                          = "ambsqllogs${var.env_name}"
+  resource_group_name           = azurerm_resource_group.amb_sql_rg.name
+  location                      = azurerm_resource_group.amb_sql_rg.location
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
+  min_tls_version               = "TLS1_2"
+  public_network_access_enabled = false
+
+  queue_properties {
+    logging {
+      delete                = true
+      read                  = true
+      write                 = true
+      version               = "1.0"
+      retention_policy_days = 100
+    }
+  }
+
+  tags = {
+    tag = var.exampletag
+  }
+}
+
+resource "azurerm_mssql_server" "amb_sql_server" {
+  name                          = "amb-sql-server-${var.env_name}"
+  resource_group_name           = azurerm_resource_group.amb_sql_rg.name
+  location                      = azurerm_resource_group.amb_sql_rg.location
+  version                       = "12.0"
+  public_network_access_enabled = false
+  minimum_tls_version           = "1.2"
+
+  azuread_administrator {
+    azuread_authentication_only = true
+    login_username              = var.admin_upn
+    object_id                   = data.azuread_user.admin.object_id
+  }
+
+  tags = {
+    tag = var.exampletag
+  }
+}
+
+resource "azurerm_mssql_database" "amb_sql_database" {
+  name           = "amb-sql-database-${var.env_name}"
+  server_id      = azurerm_mssql_server.amb_sql_server.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  sku_name       = "S0"
+
+  tags = {
+    tag = var.exampletag
+  }
+}
--- infra/main.tf
+++ infra/main.tf
@@ -12,6 +12,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.46.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=2.36.0"
+    }
   }
 }
 
@@ -19,4 +23,8 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 
   features {}
+}
+
+provider "azuread" {
+  tenant_id = var.tenant_id
 }
