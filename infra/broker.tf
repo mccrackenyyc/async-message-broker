ee358e5c2458b58f7d0eb4ec333fa520b1c08242
mccrackenyyc <smccracken@live.ca>
2023-03-10 16:41:08 -0700

    Refactored to use for_each loops

--- infra/broker.tf
+++ infra/broker.tf
@@ -24,35 +24,15 @@ resource "azurerm_servicebus_namespace" "amb_svcbus_ns" {
   }
 }
 
-resource "azurerm_servicebus_queue" "amb_queue_activate_customer" {
-  name                  = "amb-queue-activate-customer-${var.env_name}"
+resource "azurerm_servicebus_queue" "amb_queue" {
+  for_each              = local.queues
+  name                  = each.key
   namespace_id          = azurerm_servicebus_namespace.amb_svcbus_ns.id
   max_size_in_megabytes = 1024
 }
 
-resource "azurerm_servicebus_queue" "amb_queue_deactivate_customer" {
-  name                  = "amb-queue-deactivate-customer-${var.env_name}"
-  namespace_id          = azurerm_servicebus_namespace.amb_svcbus_ns.id
-  max_size_in_megabytes = 1024
-}
-
-resource "azurerm_servicebus_queue" "amb_queue_email_customer" {
-  name                  = "amb-queue-email-customer-${var.env_name}"
-  namespace_id          = azurerm_servicebus_namespace.amb_svcbus_ns.id
-  max_size_in_megabytes = 1024
-}
-
-resource "azurerm_servicebus_topic" "amb_topic_customer_created" {
-  name         = "amb-topic-customer-created-${var.env_name}"
-  namespace_id = azurerm_servicebus_namespace.amb_svcbus_ns.id
-}
-
-resource "azurerm_servicebus_topic" "amb_topic_customer_updated" {
-  name         = "amb-topic-customer-updated-${var.env_name}"
-  namespace_id = azurerm_servicebus_namespace.amb_svcbus_ns.id
-}
-
-resource "azurerm_servicebus_topic" "amb_topic_region_added" {
-  name         = "amb-topic-region-added-${var.env_name}"
+resource "azurerm_servicebus_topic" "amb_topic" {
+  for_each     = local.topics
+  name         = each.key
   namespace_id = azurerm_servicebus_namespace.amb_svcbus_ns.id
 }
--- infra/consumer.tf
+++ infra/consumer.tf
@@ -49,24 +49,9 @@ resource "azurerm_service_plan" "amb_email_function_plan" {
   }
 }
 
-resource "azurerm_linux_function_app" "amb_function_send_email" {
-  name                = "amb-function-send-email-${var.env_name}"
-  resource_group_name = azurerm_resource_group.amb_function_rg.name
-  location            = azurerm_resource_group.amb_function_rg.location
-
-  storage_account_name       = azurerm_storage_account.amb_function_storage.name
-  storage_account_access_key = azurerm_storage_account.amb_function_storage.primary_access_key
-  service_plan_id            = azurerm_service_plan.amb_email_function_plan.id
-
-  site_config {
-    application_stack {
-      python_version = "3.10"
-    }
-  }
-}
-
-resource "azurerm_linux_function_app" "amb_function_update_customer_records" {
-  name                = "amb-function-update-customer-records-${var.env_name}"
+resource "azurerm_linux_function_app" "amb_function" {
+  for_each            = local.functions
+  name                = each.key
   resource_group_name = azurerm_resource_group.amb_function_rg.name
   location            = azurerm_resource_group.amb_function_rg.location
 
