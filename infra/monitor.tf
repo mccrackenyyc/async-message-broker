57e3cf7ea7742b6928fbf17d84d29bad8ec16a09
mccrackenyyc <smccracken@live.ca>
2023-03-14 15:12:53 -0600

    Updates to monitoring

--- infra/monitor.tf
+++ infra/monitor.tf
@@ -18,21 +18,29 @@ resource "azurerm_monitor_action_group" "amb_monitor_group_main" {
   }
 }
 
-resource "azurerm_monitor_metric_alert" "amb_alert_queue_topic_volume_zero" {
-  name                = "amb-alert-queue-topic-volume-zero-${var.env_name}"
-  resource_group_name = azurerm_resource_group.amb_monitor_rg.name
-  scopes              = [azurerm_servicebus_namespace.amb_svcbus_ns.id]
-  description         = "Monitors if queue/topic volumes become zero which could indicate communication failures"
+# Commented out due to bug/limitation in building monitor
+# https://github.com/hashicorp/terraform-provider-azurerm/issues/20944
+# resource "azurerm_monitor_metric_alert" "amb_alert_queue_topic_volume_irregular" {
+#   name                = "amb-alert-queue-topic-volume-irregular-${var.env_name}"
+#   resource_group_name = azurerm_resource_group.amb_monitor_rg.name
+#   scopes              = [azurerm_servicebus_namespace.amb_svcbus_ns.id]
+#   description         = "Monitors if queue or topic volumes become irregular"
 
-  criteria {
-    metric_namespace = "microsoft.servicebus/namespaces"
-    metric_name      = "Messages"
-    aggregation      = "Maximum"
-    operator         = "LessThan"
-    threshold        = 1
-  }
+#   dynamic_criteria {
+#     metric_namespace  = "microsoft.servicebus/namespaces"
+#     metric_name       = "ActiveMessages"
+#     aggregation       = "Average"
+#     operator          = "GreaterOrLessThan"
+#     alert_sensitivity = "High"
 
-  action {
-    action_group_id = azurerm_monitor_action_group.amb_monitor_group_main.id
-  }
-}
+#     dimension {
+#       name     = "EntityName"
+#       operator = "Include"
+#       values   = ["*"]
+#     }
+#   }
+
+#   action {
+#     action_group_id = azurerm_monitor_action_group.amb_monitor_group_main.id
+#   }
+# }
